f291292b1234567caececcc34ca6e4f7964a61bf
Marcin <marcinkukulka@gmail.com>
2023-02-05 02:56:05 +0100

    added colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        colors: {
+            'neutral/10': '#FFFFFF',
+            'neutral/20': '#F3F3F3',
+            'neutral/40': '#D9D9D9',
+            'neutral/70': '#848484',
+            'neutral/80': '#636B75',
+            'neutral/90': '#333333',
+            'primary/20': '#CEDBD4',
+            'primary/40': '#749E87',
+            'primary/60': '#508468',
+            'primary/80': '#375B48',
+            'primary/90': '#1F593A',
+            'secondary/10': '#d4ddbf',
+            'secondary/60': '#8FBF26',
+            'secondary/100': '#445B12',
+        },
         extend: {},
     },
     plugins: [],
