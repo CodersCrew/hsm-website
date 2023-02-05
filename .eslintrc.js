a312d998810cba48d5b924f036e1ff6eb7e3f489
Marcin <marcinkukulka@gmail.com>
2023-02-05 02:03:04 +0100

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         project: TSCONFIG_PROJECTS,
         tsconfigRootDir: __dirname,
     },
+    plugins: ['@typescript-eslint', 'simple-import-sort'],
     extends: [
         'next/core-web-vitals',
         'eslint:recommended',
@@ -24,13 +25,19 @@ module.exports = {
     rules: {
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'off',
+        'import/no-unresolved': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
     },
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
         },
         'import/resolver': {
-            typescript: {},
+            node: {
+                paths: ['src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
         },
     },
 };
