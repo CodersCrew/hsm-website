f115033f8f1664af5c90cf04529c3eda5e493dc1
Sebastian Sloniec <sebastian.sloniec@start-up.house>
2023-02-15 17:15:33 +0100

    use @next/font for applying font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import fontLato from '@/fonts/lato';
+
 export default function Home() {
     return (
         <>
@@ -9,7 +11,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>
+            <main className={fontLato.className}>
                 <h1 className="text-2xl">Hello HSM!</h1>
             </main>
         </>
