e0dbda223048ef9aa87d314c224a0d19fd40f417
Sebastian Sloniec <sebastian.sloniec@start-up.house>
2023-02-18 17:50:54 +0100

    Lint project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 import fontLato from '@/fonts/lato';
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body className={fontLato.className}>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head />
+      <body className={fontLato.className}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
