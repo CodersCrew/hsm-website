fe8f78d7f89079f4d5bdb6e175340e0c92cce344
Sebastian Sloniec <sebastian.sloniec@start-up.house>
2023-02-28 19:37:56 +0100

    feat(format-e1): remove unneeded div

--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -13,11 +13,9 @@ export const TopSection: React.FC<TopSectionProps> = ({ description, header, ima
   return (
     <div className="w-full items-center bg-primary/90 md:flex md:flex-row-reverse">
       <Image src={image.src} height={500} width={500} alt={image.alt} className="w-full md:max-w-lg" />
-      <div className=" w-full max-w-7xl">
-        <div className="mr-auto px-4 pb-8 pt-10 text-neutral/20 md:text-lg lg:text-2xl">
-          <h1 className="mb-6 text-3xl font-bold lg:text-5xl">{header}</h1>
-          <p className="">{description}</p>
-        </div>
+      <div className="mr-auto px-4 pb-8 pt-10 text-neutral/20 md:text-lg lg:pl-16 lg:text-2xl">
+        <h1 className="mb-6 text-3xl font-bold lg:text-5xl">{header}</h1>
+        <p className="">{description}</p>
       </div>
     </div>
   );
