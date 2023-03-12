f279042f8be32eb58821e59fb44fce9d1a24586f
Marcin <marcinkukulka@gmail.com>
2023-03-12 14:22:12 +0100

    fix: added proper nav items import

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,17 +3,8 @@ import { useEffect, useState } from 'react';
 import BurgerMenuSrc from '../../common/assets/navbar/burger-menu.png';
 import { BurgerMenu } from './BurgerMenu';
 import { Logo } from './Logo';
+import { NAV_ITEMS } from './Navbar.constants';
 import { NavItem } from './NavItem';
-
-const NavItems = [
-  { id: 0, navItem: 'O nas', navSrc: '/about' },
-  { id: 1, navItem: 'Projekty', navSrc: '/projects' },
-  { id: 2, navItem: 'Format E1', navSrc: '/formate1' },
-  { id: 3, navItem: 'Współpraca', navSrc: '/partnership' },
-  { id: 4, navItem: 'Dołącz do nas', navSrc: '/connect' },
-  { id: 5, navItem: 'Kontakt', navSrc: '/contact' },
-  { id: 6, navItem: 'PL', navSrc: '/language' },
-];
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -35,8 +26,8 @@ export const Navbar = () => {
         <ul
           className={`fixed top-[56px] z-10 flex h-full w-full flex-col items-center space-y-4 bg-neutral/10 pt-3 transition-transform ${toggleClassNav} text-[16px] md:static  md:h-[56px] md:w-auto md:transform-none md:flex-row md:items-center md:gap-x-4 md:space-y-0 md:pr-4 lg:gap-8`}
         >
-          {NavItems.map((item) => (
-            <NavItem navItemName={item.navItem} navItemSrc={item.navSrc} key={item.id} />
+          {NAV_ITEMS.map((item) => (
+            <NavItem navItemName={item.navItemName} navItemSrc={item.navItemSrc} key={item.id} />
           ))}
         </ul>
       </div>
