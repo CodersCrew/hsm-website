f6cf29e7f09860767db3d64190a0d2206efbb136
sebslon <71407958+sebslon@users.noreply.github.com>
2023-03-26 14:13:45 +0200

    page(dolacz-do-nas): basic implementation (#34)
    Co-authored-by: Sebastian Sloniec <sebastian.sloniec@start-up.house>

--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
+import { PropsWithChildren } from 'react';
 
-interface TopSectionProps {
+interface TopSectionProps extends PropsWithChildren {
   image: {
     src: string;
     alt: string;
@@ -9,12 +10,15 @@ interface TopSectionProps {
   description: string;
 }
 
-export const TopSection: React.FC<TopSectionProps> = ({ description, header, image }) => (
+export const TopSection: React.FC<TopSectionProps> = ({ description, header, image, children }) => (
   <div className="w-full items-center bg-primary/90 md:flex md:flex-row-reverse">
     <Image src={image.src} height={500} width={500} alt={image.alt} className="w-full md:max-w-lg" />
     <div className="mr-auto px-4 pb-8 pt-10 text-neutral/20 md:text-lg lg:pl-16 lg:text-2xl">
+      {/* todo: refactor top section */}
+      {/* todo: add everything as a children */}
       <h1 className="mb-6 text-3xl font-bold lg:text-5xl">{header}</h1>
       <p className="">{description}</p>
+      {children}
     </div>
   </div>
 );
