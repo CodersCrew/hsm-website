2e72b2000c4ef5f6a474a76cdade1c6eaaf3091b
Sebastian Sloniec <sebastian.sloniec@start-up.house>
2023-04-17 21:48:28 +0200

    fix(navigation/contribution): stretch background to screen width

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -23,23 +23,25 @@ export const Navigation = () => {
   }, [router.events]);
 
   return (
-    <nav className="mx-auto flex w-full max-w-7xl flex-col bg-neutral/10 md:flex-row md:justify-between">
-      <Logo>
-        <BurgerMenu toggleNav={toggleNav} />
-      </Logo>
-      <ul
-        className={`
+    <div className="bg-neutral/10">
+      <nav className="mx-auto flex w-full max-w-7xl flex-col md:flex-row md:justify-between">
+        <Logo>
+          <BurgerMenu toggleNav={toggleNav} />
+        </Logo>
+        <ul
+          className={`
           fixed top-[60px] z-10 flex h-full w-full flex-col 
           items-center space-y-4 bg-neutral/10 pt-3 transition-transform ${open ? 'translate-x-0' : 'translate-x-full'} 
           md:static md:w-auto md:transform-none md:flex-row md:items-center 
           md:gap-x-4 md:space-y-0 md:pr-4 lg:gap-8`}
-      >
-        {NAV_LINKS.map((item) => {
-          const { text, path } = item;
+        >
+          {NAV_LINKS.map((item) => {
+            const { text, path } = item;
 
-          return <NavigationLink onClick={() => setOpne(false)} text={text} path={path} key={path} />;
-        })}
-      </ul>
-    </nav>
+            return <NavigationLink text={text} path={path} key={path} />;
+          })}
+        </ul>
+      </nav>
+    </div>
   );
 };
