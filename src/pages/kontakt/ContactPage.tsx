87866c5fb55fea57ee910a0544587533f747fe2f
MaStatek <mateusz.statkiewicz@coderscrew.pl>
2023-05-12 22:44:19 +0200

    Embedded map implemented

--- src/pages/kontakt/ContactPage.tsx
+++ src/pages/kontakt/ContactPage.tsx
@@ -19,7 +19,16 @@ const ContactPage = () => (
           Zobacz na mapie <Image src={GreenArrowIcon} height={15} width={15} alt="arrow icon" className="my-4 ml-2" />
         </Link>
       </div>
-      <Image src="/images/kontakt/mapa.png" height={325} width={588} alt="map" className="hidden lg:block" />
+      <iframe
+        title="location on map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2504.4512134455213!2d17.056062600000004!3d51.1185847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9d1f8e9739b%3A0x48731e48c728de15!2sG%C3%B3rnickiego%2022%2C%2050-337%20Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1683911563195!5m2!1spl!2spl"
+        width="588"
+        height="325"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
+
     </div>
     <div>
       <h3 className="my-4 text-2xl lg:text-primary/60">Zapraszamy do kontaktu</h3>
